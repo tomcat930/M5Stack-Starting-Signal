53969d37162a6abad469ead540df7b0e7a180723
Tomohiro Murayama <silhouette.formula1@gmail.com>
2023-08-19 03:54:41 +0900

    Improve readability of multi-line command

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,24 @@ const int beep_volume = 3;
 unsigned long current_time;
 unsigned long start_time;
 unsigned long count_time;
-
-int buttun_push_time;
+unsigned long buttun_push_time;
 
 int count_mode = 0;
 
-const int count_down_mode = 1;
-const int count_up_mode = 2;
-const int count_stop_mode = 3;
+const int countdown_mode = 1;
+const int countup_mode = 2;
+const int countstop_mode = 3;
 
-int count_stop_time = 0;
-int count_stop_start_time = 0;
-int count_stop_end_time = 0;
+int countstop_time = 0;
+int countstop_enable_time = 0;
+int countstop_disable_time = 0;
 
 int mm;
 int ss;
 int ms;
 
-const int count_down_xpos = 128;
-const int count_down_ypos = 120;
+const int countdown_xpos = 128;
+const int countdown_ypos = 120;
 
 int signal_xpos;
 int signal_ypos = 60;
@@ -52,12 +51,10 @@ void btnCdisplayText(String text) {
 }
 
 void drawStartDisplay() {
-  // title display setup
   M5.Lcd.setCursor(31, 110, 1);
   M5.Lcd.setTextColor(WHITE, BLACK);
   M5.Lcd.setTextSize(3);
   M5.Lcd.print("COUNT START APP");
-  // btnA display setup
   btnAdisplayText("START");
 }
 
@@ -87,8 +84,8 @@ void drawTimeDisplay(unsigned long time) {
 
 void resetCount() {
   count_mode = 0;
-  count_stop_time = 0;
-  count_time = count_stop_mode;
+  countstop_time = 0;
+  count_time = countstop_mode;
   M5.Lcd.fillScreen(TFT_BLACK);
   drawStartDisplay();
 }
@@ -105,79 +102,78 @@ void loop() {
   current_time = millis();
 
   if (M5.BtnA.wasPressed() && count_mode == 0) {
-    count_mode = count_down_mode;
+    count_mode = countdown_mode;
     count_time = 3;
     buttun_push_time = current_time;
     M5.Lcd.fillScreen(TFT_BLACK);
-    // count down display setup
     M5.Lcd.setTextColor(WHITE, BLACK);
     M5.Lcd.setTextFont(7);
     M5.Lcd.setTextSize(2);
   }
 
   // timer stop
-  if (M5.BtnA.wasPressed() && count_mode == count_up_mode) {
-    count_stop_start_time = current_time;
-    count_mode = count_stop_mode;
+  if (M5.BtnA.wasPressed() && count_mode == countup_mode) {
+    countstop_enable_time = current_time;
+    count_mode = countstop_mode;
     btnAdisplayText("RESTART");
     btnCdisplayText("RESET");
-  } else if (M5.BtnA.wasPressed() && count_mode == count_stop_mode) {
-    count_mode = count_up_mode;
-    count_stop_end_time = current_time;
-    count_stop_time =
-        count_stop_end_time - count_stop_start_time + count_stop_time;
+  } else if (M5.BtnA.wasPressed() && count_mode == countstop_mode) {
+    count_mode = countup_mode;
+    countstop_disable_time = current_time;
+    countstop_time =
+        countstop_disable_time - countstop_enable_time + countstop_time;
     M5.Lcd.fillScreen(TFT_BLACK);
   }
 
-  if (M5.BtnC.wasPressed() && count_mode == count_up_mode) {
+  if (M5.BtnC.wasPressed() && count_mode == countup_mode) {
     resetCount();
   }
 
-  if (M5.BtnC.wasPressed() && count_mode == count_stop_mode) {
+  if (M5.BtnC.wasPressed() && count_mode == countstop_mode) {
     resetCount();
   }
 
   switch (count_mode) {
-    case count_down_mode:
+    case countdown_mode:
       if (current_time == buttun_push_time + 1000) {
         M5.Lcd.fillCircle(70, signal_ypos, signal_size, RED);
-        M5.Lcd.setCursor(count_down_xpos, count_down_ypos, 7);
+        M5.Lcd.setCursor(countdown_xpos, countdown_ypos, 7);
         M5.Lcd.print(count_time);
         beep(100);
 
       } else if (current_time == buttun_push_time + 2000) {
         M5.Lcd.fillCircle(160, signal_ypos, signal_size, RED);
-        M5.Lcd.setCursor(count_down_xpos, count_down_ypos, 7);
+        M5.Lcd.setCursor(countdown_xpos, countdown_ypos, 7);
         M5.Lcd.print(--count_time);
         beep(100);
 
       } else if (current_time == buttun_push_time + 3000) {
         M5.Lcd.fillCircle(250, signal_ypos, signal_size, RED);
-        M5.Lcd.setCursor(count_down_xpos, count_down_ypos, 7);
+        M5.Lcd.setCursor(countdown_xpos, countdown_ypos, 7);
         M5.Lcd.print(--count_time);
         beep(100);
 
       } else if (current_time == buttun_push_time + 4000) {
         start_time = current_time;
         M5.Lcd.fillScreen(TFT_BLACK);
-        M5.Lcd.setCursor(count_down_xpos, count_down_ypos, 7);
+        M5.Lcd.setCursor(countdown_xpos, countdown_ypos, 7);
         M5.Lcd.print(--count_time);
         beep(400);
 
         M5.Lcd.fillScreen(TFT_BLACK);
-        count_mode = count_up_mode;
+        count_mode = countup_mode;
       }
       break;
 
-    case count_up_mode:
+    case countup_mode:
       count_time = current_time - start_time;
-      count_time -= count_stop_time;
+      count_time -= countstop_time;
       btnAdisplayText("STOP");
       btnCdisplayText("RESET");
       drawTimeDisplay(count_time);
       break;
 
-    case count_stop_mode:
+    case countstop_mode:
       drawTimeDisplay(count_time);
       break;
 
